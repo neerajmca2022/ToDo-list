0ee15dc495c2f3430c9e958c8579c91287e8874a
neeraj singh <neerajrajpoot93@gmail.com>
2022-01-27 14:21:14 +0530

    Initial Commit

---
+++ css/app.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const App = () => {
+return (
+ <>
+    <div classname="main_div">
+    <div classname="center_div">
+    <br />
+    <h1>ToDo list</h1>
+    <br />
+    <input type="text" placeholder="Add a items"/>
+    <button> + </button>
+
+    <ol>
+        <li>  buy apple </li>
+    </ol>
+    </div>
+
+);
+}
---
+++ css/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDoM from "react-dom";
+import "./index.css";
+import App from "./App";
+import Demo from "./Demo";
+
+ReactDoM.render(<App />, document.getElementById("root"));
